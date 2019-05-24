ceedc4a0ba1deafb16909640019a7c3e67a0b606
Adrian Lewkowicz <adrian.lewkowicz.biala@gmail.com>
2019-05-24 13:50:13 +0200

    final

--- Newton/Form1.cs
+++ Newton/Form1.cs
@@ -16,5 +16,74 @@ namespace Newton
         {
             InitializeComponent();
         }
+        double fourPower;
+        double threePower;
+        double twoPower;
+        double onePower;
+        double C;
+        double A;
+        double B;
+        double epsilon;
+        double[] x = new double[100];
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            initializeValues();
+            if (FunkcjaOblicz(A)*FunkcjaOblicz(B)<0)
+            {
+                if(FunkcjaOblicz(A)<0 && FunkcjaOblicz(B)>0 && pochodnaDruga(B) > 0)
+                {
+                    x[0] = A;
+                }
+                else if (FunkcjaOblicz(A) < 0 && FunkcjaOblicz(B) > 0 && pochodnaDruga(A) < 0)
+                {
+                    x[0] = B;
+                }
+                else if (FunkcjaOblicz(A) > 0 && FunkcjaOblicz(B) < 0 && pochodnaDruga(A) > 0)
+                {
+                    x[0] = B;
+                }
+                else if (FunkcjaOblicz(A) > 0 && FunkcjaOblicz(B) > 0 && pochodnaDruga(B) < 0)
+                {
+                    x[0] = A;
+                }
+                int i = 0;
+                do
+                {
+                    x[i + 1] = x[i] - FunkcjaOblicz(x[i]) / pochodnaPierwsza(x[i]);
+                    i++;
+                } while (Math.Abs(x[i] - x[i - 1]) > epsilon);
+                EqualsTextBox.Text = x[i].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Nie poprawny przedział");
+            }
+        }
+        double FunkcjaOblicz(double x)
+        {
+            double funckja = fourPower * Math.Pow(x, 4) + threePower * Math.Pow(x, 3) + twoPower * Math.Pow(x, 2) + onePower * Math.Pow(x, 1) + C;
+            return funckja;
+        }
+        double pochodnaPierwsza(double x)
+        {
+            double pochodna = fourPower * 4 * Math.Pow(x, 3) + threePower * 3 * Math.Pow(x, 2) + twoPower * 2 * x + onePower;
+            return pochodna;
+        }
+        double pochodnaDruga(double x)
+        {
+            return 4 * 3 * Math.Pow(x, 2) + 3 * 2 * x + twoPower;
+        }
+        void initializeValues()
+        {
+            A = (double)AnumericUpDown.Value;
+            B = (double)BnumericUpDown.Value;
+            fourPower = (double)fourPowernumericUpDown.Value;
+            threePower = (double)ThreePowernumericUpDown.Value;
+            twoPower = (double)TwonumericUpDown.Value;
+            onePower = (double)PowernumericUpDown.Value;
+            C = (double)CNumericUpDown.Value;
+            epsilon = (double)EPSnumericUpDown.Value;
+        }
+
     }
 }
